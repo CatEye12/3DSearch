aaf3c4ca3253afeaee8bcf5aba697ef5e3a2758a
CatEye12 <dashulechka20@yandex.ru>
2018-12-15 20:46:21 +0200

    Add files via upload
    Скрипт для БД

---
+++ dbo.KindaRepository.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[KindaRepository] (
+    [Id]      INT            IDENTITY (1, 1) NOT NULL,
+    [Size1]   DECIMAL (18)   NOT NULL,
+    [Size2]   DECIMAL (18)   NOT NULL,
+    [Size3]   DECIMAL (18)   NOT NULL,
+    [Model]   IMAGE          NOT NULL,
+    [Path]    NVARCHAR (255) NOT NULL,
+    [DimVal1] DECIMAL (18)   DEFAULT ((0)) NOT NULL,
+    [DimVal2] DECIMAL (18)   DEFAULT ((0)) NOT NULL,
+    [DimVal3] DECIMAL (18)   DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
